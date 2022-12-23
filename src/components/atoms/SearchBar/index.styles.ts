eaea8c56f27cef7b650c9ea80a4d94503a08f3f7
YEACHAN <everever1275@gmail.com>
2022-12-24 02:23:40 +0900

    헤더 검색창 돋보기 위치수정

--- src/components/atoms/SearchBar/index.styles.ts
+++ src/components/atoms/SearchBar/index.styles.ts
@@ -8,19 +8,21 @@ export const styles = {
     align-items: center;
     position: relative;
     border-radius: 6px;
-    background-color: #f8f9fc;
+    //background-color: #f8f9fc;
+    background-color: white;
+    margin-bottom: 20px;
   `,
   searchBar: css`
     width: 100%;
-    height: 24px;
+    height: 20px;
     text-align: left;
     background-color: transparent;
-    margin-left: 11%;
+    margin-left: 4%;
     border: 0;
     color: black;
   `,
   img: css`
     margin-left: 8px;
-    position: absolute;
+    margin-right: 4.5%;
   `,
 };
