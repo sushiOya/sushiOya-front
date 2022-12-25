9fa49f67778b74f59a6ef0ccdcc6a54cb498f42a
falsystack <falsystack@gmail.com>
2022-12-25 23:55:41 +0900

    메인페이지 컴포넌트 분리, 비즈니스 로직 분리

---
+++ src/components/molecules/ImagesViewCard/index.tsx
@@ -0,0 +1,30 @@
+import Text, { FONT_WEIGHT, TEXT } from '../../atoms/Text';
+import Title, { TITLE } from '../../atoms/Title';
+import { Restaurant } from '../../pages/MainPage';
+import BookMarkImageView from '../BookmarkImageView';
+import Card from '../Card';
+
+type Props = {
+  restaurant: Restaurant;
+};
+
+const ImageViewCard = ({
+  restaurant: { name, location, imagePath },
+}: Props): JSX.Element => {
+  return (
+    <Card>
+      <BookMarkImageView
+        marked={false}
+        onBookmarkClicked={() => {}}
+        alt={'sushi'}
+        imagePath={imagePath}
+      />
+      <Title as={TITLE.H4}>{name}</Title>
+      <Text as={TEXT.BODY1} styleProps={{ fontWeight: FONT_WEIGHT.REGULAR }}>
+        {location}
+      </Text>
+    </Card>
+  );
+};
+
+export default ImageViewCard;
--- src/components/pages/MainPage/index.styles.ts
+++ src/components/pages/MainPage/index.styles.ts
@@ -1,13 +1,7 @@
 import { css } from '@emotion/react';
 
 export const styles = {
-  recentOmakaseWrapper: css`
-    width: 304px;
-    height: 34px;
-    margin-bottom: 16px;
-  `,
-
-  bodyWrapper: css`
+  container: css`
     margin: 16px 0 16px 16px;
   `,
 };
